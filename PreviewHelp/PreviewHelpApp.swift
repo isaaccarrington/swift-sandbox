8bba3ea24327c88a46915c74210e989d2090ec71
Isaac Carrington <i_carrington@outlook.com>
2023-10-01 21:10:33 +1300

    Initial Commit

---
+++ PreviewHelp/PreviewHelpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewHelpApp.swift
+//  PreviewHelp
+//
+//  Created by Isaac Carrington on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PreviewHelpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
